1dde1ca9aee00be031021fd164a5a74747425e70
RianCahyadi08 <ryancahyadi11@gmail.com>
2023-10-15 21:56:11 +0700

    Create widget header & carousel slider

--- lib/widgets/product_card.dart
+++ lib/widgets/product_card.dart
@@ -6,49 +6,54 @@ class ProductCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 215,
-      // height: 278,
-      margin: EdgeInsets.only(right: defaultMargin),
-      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(20),
-        color: const Color(0xffECEFEF),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Image.asset(
-            'assets/image_6.png',
-            width: 215,
-            height: 150,
-            fit: BoxFit.cover,
-          ),
-          Text(
-            'Hiking',
-            style: secondaryTextStyle.copyWith(
-              fontSize: 12,
+    return GestureDetector(
+      onTap: () {
+        Navigator.pushNamed(context, '/product');
+      },
+      child: Container(
+        width: 215,
+        // height: 278,
+        margin: EdgeInsets.only(right: defaultMargin),
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(20),
+          color: const Color(0xffECEFEF),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Image.asset(
+              'assets/image_6.png',
+              width: 215,
+              height: 150,
+              fit: BoxFit.cover,
             ),
-          ),
-          const SizedBox(height: 6),
-          Text(
-            'Court vision 2.0'.toUpperCase(),
-            style: nameProductStyle.copyWith(
-              fontSize: 18,
-              fontWeight: semiBold,
-              letterSpacing: 0.18,
+            Text(
+              'Hiking',
+              style: secondaryTextStyle.copyWith(
+                fontSize: 12,
+              ),
             ),
-            overflow: TextOverflow.ellipsis,
-          ),
-          const SizedBox(height: 6),
-          Text(
-            '\$58,67',
-            style: priceTextStyle.copyWith(
-              fontSize: 14,
-              fontWeight: medium,
+            const SizedBox(height: 6),
+            Text(
+              'Court vision 2.0'.toUpperCase(),
+              style: nameProductStyle.copyWith(
+                fontSize: 18,
+                fontWeight: semiBold,
+                letterSpacing: 0.18,
+              ),
+              overflow: TextOverflow.ellipsis,
             ),
-          ),
-        ],
+            const SizedBox(height: 6),
+            Text(
+              '\$58,67',
+              style: priceTextStyle.copyWith(
+                fontSize: 14,
+                fontWeight: medium,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
