6e423b250a98a2fbd7489a7b126a5969773e6e41
RianCahyadi08 <ryancahyadi11@gmail.com>
2023-10-19 22:52:30 +0700

    Create widget content

--- lib/pages/product_page.dart
+++ lib/pages/product_page.dart
@@ -73,6 +73,7 @@ class _ProductPageState extends State<ProductPage> {
                 )
                 .toList(),
             options: CarouselOptions(
+              enableInfiniteScroll: false,
               initialPage: 0,
               onPageChanged: (index, reason) {
                 setState(() {
@@ -95,11 +96,133 @@ class _ProductPageState extends State<ProductPage> {
       );
     }
 
+    Widget content() {
+      return Container(
+        margin: const EdgeInsets.only(top: 17),
+        // padding: const EdgeInsets.all(30),
+        // height: MediaQuery.of(context).size.height,
+        width: double.infinity,
+        decoration: BoxDecoration(
+          color: backgroundColor1,
+          borderRadius: const BorderRadius.vertical(
+            top: Radius.circular(24),
+          ),
+        ),
+        child: Column(
+          children: [
+            Container(
+              margin: const EdgeInsets.only(
+                top: 30,
+                left: 30,
+                right: 30,
+              ),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'TERREX URBAN LOW',
+                          style: primaryTextStyle.copyWith(
+                              fontSize: 18,
+                              fontWeight: semiBold,
+                              letterSpacing: 0.18),
+                        ),
+                        Text(
+                          'Hiking',
+                          style: subtitleTextStyle.copyWith(
+                            fontSize: 12,
+                            fontWeight: reguler,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Image.asset(
+                    'assets/btn_wishlist.png',
+                    width: 46,
+                    height: 46,
+                  ),
+                ],
+              ),
+            ),
+            // Note: Price
+            Container(
+              width: double.infinity,
+              margin: const EdgeInsets.only(
+                top: 20,
+                left: 30,
+                right: 30,
+              ),
+              padding: const EdgeInsets.all(16),
+              decoration: BoxDecoration(
+                color: backgroundColor2,
+                borderRadius: BorderRadius.circular(4),
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'Price starts from',
+                    style: primaryTextStyle.copyWith(
+                      fontSize: 14,
+                      fontWeight: reguler,
+                    ),
+                  ),
+                  Text(
+                    '\$143,98',
+                    style: priceTextStyle.copyWith(
+                      fontSize: 16,
+                      fontWeight: semiBold,
+                    ),
+                  )
+                ],
+              ),
+            ),
+            // Description
+            Container(
+              width: double.infinity,
+              margin: const EdgeInsets.only(
+                top: 30,
+                left: 30,
+                right: 30,
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Description',
+                    style: primaryTextStyle.copyWith(
+                      fontWeight: medium,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 12,
+                  ),
+                  Text(
+                    'Unpaved trails and mixed surfaces are easy when you have the traction and support you need. Casual enough for the daily commute.',
+                    textAlign: TextAlign.justify,
+                    style: subtitleTextStyle.copyWith(
+                      fontSize: 14,
+                      fontWeight: light,
+                      letterSpacing: 0.14,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      );
+    }
+
     return Scaffold(
       backgroundColor: backgroundColor6,
       body: ListView(
         children: [
           header(),
+          content(),
         ],
       ),
     );
